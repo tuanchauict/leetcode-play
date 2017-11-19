a4b0943a000b709ca5dd1ab192980011b0bd4ad8
Tuan Chau <tuanchauict@gmail.com>
2017-11-19 09:03:47 +0700

    Add 726

--- 726-number-of-atoms.py
+++ 726-number-of-atoms.py
@@ -1,77 +1,95 @@
 # https://leetcode.com/problems/number-of-atoms/description/
 
+
+def create_unit(formula):
+    """
+    :type formula: str
+    :rtype: str
+    """
+    arr = []
+    unit = ''
+    for c in formula:
+        if 'A' <= c <= 'Z':
+            if unit:
+                arr.append(unit)
+            unit = c
+        elif 'a' <= c <= 'z':
+            unit += c
+        elif '0' <= c <= '9':
+            if unit and unit[0] > '9':
+                arr.append(unit)
+                unit = ''
+            unit += c
+        elif c == '(' or c == ')':
+            if unit:
+                arr.append(unit)
+            arr.append(c)
+            unit = ''
+    if unit:
+        arr.append(unit)
+    return arr
+
+
 class Solution(object):
+    def sol2(self, formula):
+        """
+        :type formula: str
+        :rtype: str
+        """
+        units = create_unit(formula)
+        stack = []
+
+        for i, s in enumerate(units):
+            if s == '(':
+                stack.append(s)
+            elif s == ')':
+                if (i < len(units) - 1 and not units[i + 1].isdigit()) or i == len(units) - 1:
+                    index = len(stack) - 1
+                    while index >= 0 and stack[index] != '(':
+                        index -= 1
+                    stack.pop(index)
+                else:
+                    stack.append(s)
+            elif s.isdigit():
+                num = int(s)
+                if stack[-1] == ')':
+                    index = len(stack) - 2
+                    while index >= 0 and stack[index] != '(':
+                        stack[index][1] *= num
+                        index -= 1
+                    stack.pop(index)
+                    stack.pop(-1)
+                else:
+                    stack[-1][1] = num
+            else:
+                stack.append([s, 1])
+        m = {}
+
+        print stack
+        for (atom, number) in stack:
+            m[atom] = m.setdefault(atom, 0) + number
+        result = ''
+        keys = m.keys()
+        keys.sort()
+        for k in keys:
+            result += k + (str(m[k]) if m[k] > 1 else '')
+
+        print result
+        return result
+
     def countOfAtoms(self, formula):
         """
         :type formula: str
         :rtype: str
         """
 
-        def count(f, begin):
-
-            index = begin
-            result = {}
-
-            while index < len(f):
-                c = f[index]
-                print index, c
-                if c == '(':
-                    index, m = count(f, index + 1)
-                    print '+', index, m
-                    if index < len(f) and f[index].isdigit():
-                        num = int(f[index])
-                        print '>>', num, m
-                        for k, v in m.items():
-                            m[k] = v * num
-                        index += 1
-                    print '-', index
-                    print 'm', m, result
-
-                    for k, v in m.items():
-                        print '->', k, v
-                        result[k] = result.setdefault(k, 0) + v
-                elif c == ')':
-                    index += 1
-                    break
-                    # return index, result
-                elif c.isdigit():
-                    result[f[index - 1]] = int(c)
-                else:
-                    result[c] = 1
-                index += 1
-                # print result
-
-            return index, result
-
-        arr = []
-        unit = ''
-        for c in formula:
-            if 'A' <= c <= 'Z':
-                if unit:
-                    arr.append(unit)
-                unit = c
-            elif 'a' <= c <= 'z':
-                unit += c
-            elif '0' <= c <= '9':
-                if unit and unit[0] > '9':
-                    arr.append(unit)
-                    unit = ''
-                unit += c
-            elif c == '(' or c == ')':
-                if unit:
-                    arr.append(unit)
-                arr.append(c)
-                unit = ''
-        if unit:
-            arr.append(unit)
-        print(arr)
-
-        _, result = count(arr, 0)
-        print 'result', result
+        return self.sol2(formula)
 
 
 s = Solution()
-# s.countOfAtoms("K4(ON(SO3)2)2")
-# s.countOfAtoms("(ON(SO3)2)2")
-s.countOfAtoms("(O(O3)2)2")
-# s.countOfAtoms("Mg(OH)20")
+s.sol2("K4(ON(SO3)2)2")
+s.countOfAtoms("(ON(SO3)2)2")
+s.countOfAtoms("((O(O(O3)2)2))")
+s.sol2('O(O(O3)2)2')
+s.sol2('H2O')
+s.countOfAtoms("Mg(OH)20")
